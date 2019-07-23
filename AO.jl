085cd47c6cf206a6530be489498e6206b57d71b8
Alex <alexandershaw4@gmail.com>
2019-07-23 16:14:08 +0100

    progress

---
+++ AO.jl
@@ -0,0 +1,14 @@
+# Curvature-descent based function minimisation / model fitting / parameter estimation.
+# Based on AO.m matlab function by AS
+
+include('jaco.jl'); # for numerical differentiation
+
+function AO(P,V,M,Y,order)
+    # an approximate curvature descent routine for parameter estimation
+    # for fitting tcm models.
+
+    
+
+
+
+end
--- jaco.jl
+++ jaco.jl
@@ -16,8 +16,10 @@
 #
 
 import DataStructures
+include("dic_to_vect.jl");
 
 function jaco(fun,x,u,P,M,order)
+    # state differentiation: df/dx [Jacobian or Hessian matrix]
     f0 = fun(x,u,P,M);
     fx = copy(f0);
     j  = zeros( length(x), length(x) );
@@ -38,6 +40,48 @@ function jaco(fun,x,u,P,M,order)
 
         f0 = fun(reshape(x0,size(x)),u,P,M);
         f1 = fun(reshape(x1,size(x)),u,P,M);
+        j[i,:] = (f0 - f1) / (2d);
+
+        if order == 2
+            deriv1 = (f0 - f1) / 2 / d;
+            deriv2 = (f0 - 2 * fx + f1) / d^2;
+            j[i,:] = deriv1 ./ deriv2;
+        end
+    end
+
+    j = j';
+
+    return j, fx
+end
+
+function dpdx(x,u,P,M,order)
+    # parameter differentiation function: dp/df or dp/dx
+    f0 = fun(x,u,P,M);
+    fx = copy(f0);
+    P0 = copy(dict_to_vec(P));
+    if P0[1] == float
+        P0 = P0[2:end];
+    end
+
+    j  = zeros( length(P0), length(x) );
+    V  = copy(P0);
+
+    for i = 1:length(P0)
+
+        x0 = copy(P0);
+        x1 = copy(P0);
+
+        d  = P0[i] + V[i]*exp(-8);
+
+        if d == 0
+            d = 0.01;
+        end
+
+        x0[i] = x0[i] + d;
+        x1[i] = x1[i] - d;
+
+        f0 = fun(x,u,vec_to_dict(x0,P),M);
+        f1 = fun(x,u,vec_to_dict(x1,P),M);
         j[i,:] = (f0 - f1) / (2d);
 
         if order == 2
